e66336fe158df59ad7212eb35d949cee70f98709
Im-los <cli10@chicagobooth.edu>
2023-02-14 06:20:59 +0000

    First users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,9 +2,20 @@ class UserController <ApplicationController
 
   def index
 
+    @list_of_users = User.all
+
     render(:template => "user/index.html.erb")
   end
 
+  def user_id
+
+    the_id = params.fetch("user_id")
+    #@user_id= self_id
+    @list_of_users = User.all
+    @user_detail = @list_of_users.where(:id => the_id).first
+
+    render(:template => "user/user_detail.html.erb")
+  end 
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "home", :action => "index" })
 
   get("/users", { :controller => "user", :action => "index" })
+  get("/users/:user_id", { :controller => "user", :action => "user_id" })
 
   get("/photos", { :controller => "photo", :action => "index" })
 
